c915b3ac630cd39bd8f1e74fb191881b33f1498a
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-10 16:15:13 -0600

    setups colors, font family, and font sizes for onboarding and makes it so you can't create a username with no value

--- ZoApp/ZoApp/View Controllers/Onboarding/RulesOnboardingViewController.swift
+++ ZoApp/ZoApp/View Controllers/Onboarding/RulesOnboardingViewController.swift
@@ -10,19 +10,30 @@ import UIKit
 
 class RulesOnboardingViewController: UIViewController {
 
+    
+    // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bulletPointsLabel: UILabel!
+    @IBOutlet weak var disclaimerLabel: UILabel!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        stylizeSubviews()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - UI Adjustments
+    
+    func stylizeSubviews() {
+        view.backgroundColor = .boldGreen
+        titleLabel.font = UIFont(name: FontAttributes.h2.fontFamily, size: FontAttributes.h2.rawValue)
+        titleLabel.textColor = .zoWhite
+        bulletPointsLabel.font = UIFont(name: FontAttributes.h4.fontFamily, size: FontAttributes.h4.rawValue)
+        bulletPointsLabel.textColor = .zoWhite
+        disclaimerLabel.font = UIFont(name: FontAttributes.body.fontFamily, size: FontAttributes.body.rawValue)
+        disclaimerLabel.textColor = .zoWhite
     }
-    */
 
 }
--- ZoApp/ZoApp/View Controllers/Onboarding/UsernameOnboardingViewController.swift
+++ ZoApp/ZoApp/View Controllers/Onboarding/UsernameOnboardingViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class UsernameOnboardingViewController: UIViewController {
 
     // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var createUsernameTextField: UITextField!
+    @IBOutlet weak var disclaimerLabel: UILabel!
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
@@ -28,20 +30,36 @@ class UsernameOnboardingViewController: UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: Any) {
-        guard let username = createUsernameTextField.text, createUsernameTextField.text != nil else { return }
+        guard let username = createUsernameTextField.text, !username.isEmpty else { return }
+        
+        // MARK: - TODO
+        // TODO: - Need to check if the username already exists
+        
         UserController.shared.createUser(username: username) { (success) in
             if success {
                 print("User created.")
+                DispatchQueue.main.async {
+                    let storyboard = UIStoryboard(name: "TabController", bundle: nil)
+                    let tabViewController = storyboard.instantiateViewController(withIdentifier: "TabController")
+                    self.present(tabViewController, animated: true, completion: nil)
+                }
             } else {
                 print("User was not created.")
+                // MARK: - TODO
+                // TODO: - Create alert saying they need to enter a username
             }
         }
     }
     
+    
     // MARK: - UI Adjustments
     
     func stylizeSubviews() {
-        
+        view.backgroundColor = .boldGreen
+        titleLabel.font = UIFont(name: FontAttributes.h2.fontFamily, size: FontAttributes.h2.rawValue)
+        titleLabel.textColor = .zoWhite
+        disclaimerLabel.font = UIFont(name: FontAttributes.body.fontFamily, size: FontAttributes.body.rawValue)
+        disclaimerLabel.textColor = .zoWhite
     }
 
 }
