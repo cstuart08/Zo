761efbf46e6def8dab0501b0db314bae7ca22e2e
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-10-16 11:38:11 -0600

    adds properties to user model

--- ZoApp/ZoApp/Models/User.swift
+++ ZoApp/ZoApp/Models/User.swift
@@ -17,6 +17,8 @@ struct UserConstants {
     static let respondedToKey = "respondedToKey"
     static let savedResponsesKey = "savedResponsesKey"
     static let appleUserReferenceKey = "appleUserReferenceKey"
+    static let isBlockedKey = "isBlocked"
+    static let lastKarmaLevelIndexKey = "LastKarmaLevelIndexKey"
 }
 
 class User {
@@ -27,6 +29,8 @@ class User {
     var kpLevel: String = "Root"
     var respondedTo: [String] = ["default"]
     var savedResponses: [String] = ["default"]
+    var isBlocked = [String]()
+    var lastKarmaLevelIndex: Int = 0
     let recordID: CKRecord.ID
     let appleUserReference: CKRecord.Reference
     
@@ -43,6 +47,8 @@ class User {
             let kpLevel = ckRecord[UserConstants.kpLevelKey] as? String,
             let respondedTo = ckRecord[UserConstants.respondedToKey] as? [String],
             let savedResponses = ckRecord[UserConstants.savedResponsesKey] as? [String],
+            let lastKarmaLevelIndex = ckRecord[UserConstants.lastKarmaLevelIndexKey] as? Int,
+            let isBlocked = ckRecord[UserConstants.isBlockedKey] as? [String],
             let appleUserReference = ckRecord[UserConstants.appleUserReferenceKey] as? CKRecord.Reference else { return nil }
         
         self.username = username
@@ -52,6 +58,8 @@ class User {
         self.savedResponses = savedResponses
         self.recordID = ckRecord.recordID
         self.appleUserReference = appleUserReference
+        self.isBlocked = isBlocked
+        self.lastKarmaLevelIndex = lastKarmaLevelIndex
     }
 }
 
@@ -70,6 +78,8 @@ extension CKRecord {
         setValue(user.kpLevel, forKey: UserConstants.kpLevelKey)
         setValue(user.respondedTo, forKey: UserConstants.respondedToKey)
         setValue(user.savedResponses, forKey: UserConstants.savedResponsesKey)
+        setValue(user.isBlocked, forKey: UserConstants.isBlockedKey)
+        setValue(user.lastKarmaLevelIndex, forKey: UserConstants.lastKarmaLevelIndexKey)
         setValue(user.appleUserReference, forKey: UserConstants.appleUserReferenceKey)
     }
 }
