943b38d87885daa2377d391df9e97b44ce7d2d4b
Cameron Stuart <apps@cameronstuart.com>
2019-10-21 13:43:39 -0600

    Changes the random request images to nature images.

--- ZoApp/ZoApp/View Controllers/Requests/CreateRequestViewController.swift
+++ ZoApp/ZoApp/View Controllers/Requests/CreateRequestViewController.swift
@@ -28,7 +28,7 @@ class CreateRequestViewController: UIViewController, UITextViewDelegate {
     // MARK: - Properites
     let user = UserController.shared.currentUser
     
-    let randomImages: [UIImage] = [UIImage(named: "quote1")!, UIImage(named: "quote2")!, UIImage(named: "quote3")!, UIImage(named: "quote4")!, UIImage(named: "quote5")!, UIImage(named: "quote6")!, UIImage(named: "quote7")!, UIImage(named: "quote8")!, UIImage(named: "quote9")!, UIImage(named: "quote10")!, UIImage(named: "quote11")!, UIImage(named: "quote12")!, UIImage(named: "quote13")!]
+    let randomImages: [UIImage] = [UIImage(named: "nature1")!, UIImage(named: "nature2")!, UIImage(named: "nature3")!, UIImage(named: "nature4")!, UIImage(named: "nature5")!, UIImage(named: "nature6")!, UIImage(named: "nature7")!, UIImage(named: "nature8")!, UIImage(named: "nature9")!, UIImage(named: "nature10")!, UIImage(named: "nature11")!, UIImage(named: "nature12")!, UIImage(named: "nature13")!, UIImage(named: "nature14")!, UIImage(named: "nature15")!, UIImage(named: "nature16")!, UIImage(named: "nature17")!]
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
